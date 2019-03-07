8306dbb7275d8b6141fcb6009ef669ba9ae1804f
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-03-06 16:50:08 -0800

    Solved problem_007.

--- problem_007.cpp
+++ problem_007.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 
 
 /*Problem 7
@@ -13,11 +16,27 @@ What is the 10,001st prime number?
 
 Answer:	104743
  */
- 
 
 
-unsigned problem_007() {	
-	
+unsigned problem_007() {
+
+    const unsigned N = 10001;
+
+    std::vector<unsigned> primes(N, 1);
+    primes[0] = 2;
+    primes[1] = 3;
+
+    for(int i = 2; i < N; ++i) {
+
+        unsigned testPrime = primes[i-1] + 2;
+
+        while ( std::any_of(primes.begin(), primes.begin() + i, [testPrime] (int p) { return (testPrime % p == 0); } )) {
+            testPrime += 2;
+        }
+        primes[i] = testPrime;
+    }
+
+	return primes[N-1] ;
 }
 
 
