10ff249bedbfef00e13835e18496e5f2f5ce72d4
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-03-08 16:09:43 -0800

    Added problems 10 - 15.

---
+++ problem_015.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+/*Problem 15
+
+Lattice paths
+
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+
+
+Answer:	
+ */
+ 
+ 
+
+unsigned problem_015() {
+	
+    return 0;	
+}
+
+int main() {
+	
+   	std::cout << problem_015() << std::endl;
+
+    return 0;
+}
