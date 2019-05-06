23dd5de59b72c481cda56531214ea9aa597fbf9b
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-05-05 18:42:59 -0700

    Added new problem templates.

---
+++ problem_016.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+/*Problem 16
+
+Power digit sum
+
+
+2^16 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+
+Answer:	
+ */
+ 
+ 
+
+unsigned problem_016() {
+	
+    return 0;	
+}
+
+int main() {
+	
+   	std::cout << problem_016() << std::endl;
+
+    return 0;
+}
---
+++ problem_020.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+/*Problem 20
+
+Factorial digit sum
+
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+
+Answer:	
+ */
+ 
+ 
+
+unsigned problem_020() {
+	
+    return 0;	
+}
+
+int main() {
+	
+   	std::cout << problem_020() << std::endl;
+
+    return 0;
+}
