8deac36fe9d827e54e814eb02b76fb5f9aa45151
cholderman4 <9838791+cholderman4@users.noreply.github.com>
2019-10-22 15:06:31 -0700

    Update problem_016.cpp

--- problem_016.cpp
+++ problem_016.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
+#include <vector>
 
 /*Problem 16
 
@@ -17,11 +18,31 @@ Answer:
  
 
 unsigned problem_016() {
+
+    // MAX_DIGITS = 1000*log_1000(2) ~ 301.02
+    const unsigned MAX_DIGITS{305};
+
+    std::vector<unsigned> product(MAX_DIGITS, 0);
+    product[0]=1;
+    unsigned currentDigits = 1;
+
+    for (auto i=0; i<1000; ++i) {
+        // Loop through multiplication by 2
+        unsigned carry = 0;
+        for (auto j = 0; j < currentDigits; ++j) {
+
+        }
+
+    }
+
+
 	
     return 0;	
 }
 
 int main() {
+
+
 	
    	std::cout << problem_016() << std::endl;
 
