7aebba0a557604ab01bb3ef71eb5c160aded57e3
Charles Holderman <9838791+cholderman4@users.noreply.github.com>
2024-10-11 13:20:24 -0400

    Solve 28 (#3)

--- ProjectEuler/ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler/ProjectEuler.cpp
@@ -17,56 +17,29 @@ using namespace completed;
 
 using Int = uint64_t;
 
-int64_t quadratic_primes()
+Int number_spiral_diagonals()
 {
-    constexpr auto LIMIT = 1000u;
-    auto sieve = PrimeSieve(13 * LIMIT);
-    Int max_n = 40;
-    int64_t ab = 41;
-    int64_t b = 1;
+    constexpr Int GRIDSIZE = 1001;
+    constexpr Int GRID_RINGS = (GRIDSIZE - 1) / 2;
 
-    // By solving for n = 0 and n = 1, we can set conditions on a and b
-    while ((b = sieve.next_prime(b)) < LIMIT)
+    Int sum = 1;
+    Int n = 1;
+    for (auto ring = 1; ring <= GRID_RINGS; ++ring)
     {
-        int64_t p = 1;
-        while ((p = sieve.next_prime(p)) < LIMIT + 1 + b && b < LIMIT - 1 + p)
+        for (auto i = 0; i < 4; ++i)
         {
-            const int64_t a = p - b - 1;
-
-            // Start at 2, since we already know 0 & 1.
-            int64_t n = 2;
-            while (true)
-            {
-                const int64_t p_test = n * n + a * n + b;
-                if (p_test <= 1)
-                    break;
-
-                // From previous tests, we know we won't need to resize
-                /*if (sieve.limit() < p_test)
-                {
-                    sieve.rebuild_integer_limit(p_test);
-                }*/
-
-                if (!sieve.is_prime(p_test))
-                    break;
-                ++n;
-            }
-
-            if (n > max_n)
-            {
-                max_n = n;
-                ab = a * b;
-            }
+            n += (2 * ring);
+            sum += n;
         }
     }
 
-    return ab;
+    return sum;
 }
 
 int main()
 {
-    std::cout << quadratic_primes() << std::endl;
-    TimeFunction(quadratic_primes);
+    std::cout << number_spiral_diagonals() << std::endl;
+    //TimeFunction(quadratic_primes);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
